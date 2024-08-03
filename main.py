b8d4390d4dcf3aee84a748e449a9e94aa70554cd
Zayan <zayan_ahmad@hotmail.com>
2024-08-03 17:10:17 -0500

    Clicks in the center of the screen a 20 times

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
 import pyautogui
+import time
+import threading
+import random as rand
 
-def is_pixel_red(x, y):
+def get_center():
+    resolution = pyautogui.size()
+    return (resolution.width // 2, resolution.height // 2)
+
+def get_pixel_color(x, y):
     screen = pyautogui.screenshot()
-    pixel_color = screen.getpixel((x, y))
-    red_color = (255, 0, 0)
+    pixel_color = screen.getpixel(x, y);
+    return pixel_color
 
-    return pixel_color == red_color
 
 def main():
-    x, y = 100, 100
+
+    pixel_location = get_center()
 
     running = True
     while running:
-        if is_pixel_red(x, y):
-            print(f"The pixel at ({x}, {y}) is red")
-            running = False
+        if get_pixel_color(pixel_location) == (255, 255, 255):
+            print(f"The pixel at center is white")
+            pyautogui.moveTo(get_center())
+
+            number_of_times = 20
+
+            # Reel in
+            for i in range(1, number_of_times):
+                pyautogui.click()
+                time.sleep(0.5 + rand.random())
+
+        # Cast rod
+        pyautogui.click()
+
+        # TODO: Add fail safe to end program via key press
+        # TODO: Multi threaded approach, one thread to listen to mouse inputs and one to listen to screen inputs
+
 
 
 if __name__=='__main__':
